f7b4480d52b53616f218dbfccb08ac4a6326c9d6
xyxyLiu <tonyreginald@gmail.com>
2016-07-09 22:11:23 +0800

    modify install-test

--- install-test.sh
+++ install-test.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 pkg=cn.opda.a.phonoalbumshoushou
 
-./gradlew testplugin:assembleDebug && cp testplugin/build/outputs/apk/testplugin-debug.apk testhost/src/main/assets/ && ./gradlew testhost:assembleDebug  && adb push /baidu/PluginM/testhost/build/outputs/apk/testhost-debug.apk /data/local/tmp/com.example.testhost && adb shell pm install -r "/data/local/tmp/com.example.testhost" && adb shell am start -n "com.example.testhost/com.example.testhost.HostMainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
+./gradlew testplugin:assembleDebug && cp testplugin/build/outputs/apk/testplugin-debug.apk testhost/src/main/assets/ && ./gradlew testhost:assembleDebug  && adb push testhost/build/outputs/apk/testhost-debug.apk /data/local/tmp/com.example.testhost && adb shell pm install -r "/data/local/tmp/com.example.testhost" && adb shell am start -n "com.example.testhost/com.example.testhost.HostMainActivity" -a android.intent.action.MAIN -c android.intent.category.LAUNCHER
